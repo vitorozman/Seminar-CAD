6f8a751f9ae18156de2c45107489fdc1af76ccfa
MatejRojec <matej.4001@gmail.com>
2023-01-02 12:57:02 +0100

    c

--- Koda/primerTri.m
+++ Koda/primerTri.m
@@ -1,27 +1,27 @@
-Bx  = [1 NaN NaN NaN NaN NaN NaN;
-    2 1 NaN NaN NaN NaN NaN;
-    3 NaN 1 NaN NaN NaN NaN;
-    4 NaN NaN 1 NaN NaN NaN;
-    5 NaN NaN NaN 1 NaN NaN;
-    6 NaN NaN NaN NaN 1 NaN;
-    7 1 2 3 4 5 1];
+Bx  =  [1 NaN NaN NaN NaN NaN NaN;
+        2 1 NaN NaN NaN NaN NaN;
+        3 NaN 1 NaN NaN NaN NaN;
+        4 NaN NaN 1 NaN NaN NaN;
+        5 NaN NaN NaN 1 NaN NaN;
+        6 NaN NaN NaN NaN 1 NaN;
+        7 1 2 3 4 5 1];
 
-By = [1 NaN NaN NaN NaN NaN NaN;
-    2 4 NaN NaN NaN NaN NaN;
-    3.2 NaN 3 NaN NaN NaN NaN;
-    4.5 NaN NaN 5 NaN NaN NaN;
-    5.4 NaN NaN NaN 7 NaN NaN;
-    6.7 NaN NaN NaN NaN 10 NaN;
-    7.3 11 2 3 8 5 1];
+By =   [1 NaN NaN NaN NaN NaN NaN;
+        2 4 NaN NaN NaN NaN NaN;
+        3.2 NaN 3 NaN NaN NaN NaN;
+        4.5 NaN NaN 5 NaN NaN NaN;
+        5.4 NaN NaN NaN 7 NaN NaN;
+        6.7 NaN NaN NaN NaN 10 NaN;
+        7.3 11 2 3 8 5 1];
 
 
-Bz = [1 NaN NaN NaN NaN NaN NaN;
-    3 3 NaN NaN NaN NaN NaN;
-    2 NaN 7 NaN NaN NaN NaN;
-    6 NaN NaN 9 NaN NaN NaN;
-    2 NaN NaN NaN 3 NaN NaN;
-    5 NaN NaN NaN NaN 5 NaN;
-    1 3 2 5 7 6 2];
+Bz =   [1 NaN NaN NaN NaN NaN NaN;
+        3 3 NaN NaN NaN NaN NaN;
+        2 NaN 7 NaN NaN NaN NaN;
+        6 NaN NaN 9 NaN NaN NaN;
+        2 NaN NaN NaN 3 NaN NaN;
+        5 NaN NaN NaN NaN 5 NaN;
+        1 3 2 5 7 6 2];
 
 
 a=0;
@@ -38,7 +38,7 @@ figure(1)
 
 trisurf(TRI,b(:,1),b(:,2),b(:,3));
 
-% nov alfa
+% Quadratic precision alfa
 
 a=-1/6;
 BX = filltriangle(Bx, a);
