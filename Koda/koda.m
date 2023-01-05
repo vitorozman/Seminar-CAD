5ee6425807db700342ec27e3d795419bdf8e13e3
vitorozman <64697421+vitorozman@users.noreply.github.com>
2023-01-05 21:47:23 +0100

    priprava

--- Koda/koda.m
+++ Koda/koda.m
@@ -1,4 +1,4 @@
-[Bx,By,Bz] = deal(zeros(9,7));
+[Bx,By,Bz] = deal(NaN(9,7));
 
 phi = linspace(0,pi,7);
 psi = linspace(0,pi,9);
@@ -14,9 +14,12 @@ Bx(:,7) = cos(psi);
 By(:,7) = 1;
 Bz(:,7) = sin(psi);
 
+Bz(1,:) = 0;
+Bz(end,:) = 0;
+
 mesh(Bx, By, Bz,  'FaceColor', 'None');
 %Kontrolne tocke Coonsove ploskve:
-[Bx,By,Bz] = coons(Bx,By,Bz);
+%[Bx,By,Bz] = coons(Bx,By,Bz);
 
 %Tocke na ploskvi:
 u = linspace(0,1,100);
@@ -25,7 +28,7 @@ v = linspace(0,1,100);
 [bx, by, bz] = bezier2(Bx,By,Bz,u,v);
 
 %graf
-surf(bx, by, bz); %nariše krivuljo na sliki oziroma vkrivljeno ploskev majhnih pravokotničkov, ki se ne prilegajo popolnoma funkciji mesh
+%surf(bx, by, bz); %nariše krivuljo na sliki oziroma vkrivljeno ploskev majhnih pravokotničkov, ki se ne prilegajo popolnoma funkciji mesh
 hold on
-mesh(Bx, By, Bz, 'FaceColor', 'None');  %prozorne pravokotnike naredi v pravi obliki
+%mesh(Bx, By, Bz, 'FaceColor', 'None');  %prozorne pravokotnike naredi v pravi obliki
 hold on
