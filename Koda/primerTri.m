0805d70a79b01b6a0fce8e6ed79d42a603f35c11
MatejRojec <matej.4001@gmail.com>
2023-01-15 11:15:00 +0100

    c

--- Koda/primerTri.m
+++ Koda/primerTri.m
@@ -1,28 +1,27 @@
-Bx  =  [1 NaN NaN NaN NaN NaN NaN;
-        2 1 NaN NaN NaN NaN NaN;
-        3 NaN 1 NaN NaN NaN NaN;
-        4 NaN NaN 1 NaN NaN NaN;
-        5 NaN NaN NaN 1 NaN NaN;
-        6 NaN NaN NaN NaN 1 NaN;
-        7 1 2 3 4 5 1];
+Bx  =  [0.5 NaN NaN NaN NaN NaN NaN;
+        -1 1.2 NaN NaN NaN NaN NaN;
+        -0.5 NaN 1.3 NaN NaN NaN NaN;
+        -0.3 NaN NaN 2.4 NaN NaN NaN;
+        0.1 NaN NaN NaN 3 NaN NaN;
+        0.4 NaN NaN NaN NaN 3.6 NaN;
+        1.2 2 2.7 3.1 3.5 3.9 4.2];
 
-By =   [1 NaN NaN NaN NaN NaN NaN;
-        2 4 NaN NaN NaN NaN NaN;
-        3.2 NaN 3 NaN NaN NaN NaN;
-        4.5 NaN NaN 5 NaN NaN NaN;
-        5.4 NaN NaN NaN 7 NaN NaN;
-        6.7 NaN NaN NaN NaN 10 NaN;
-        7.3 11 2 3 8 5 1];
+By =   [5 NaN NaN NaN NaN NaN NaN;
+        4.2 4.1 NaN NaN NaN NaN NaN;
+        3.2 NaN 3.4 NaN NaN NaN NaN;
+        2.5 NaN NaN 2.1 NaN NaN NaN;
+        2.1 NaN NaN NaN 1.7 NaN NaN;
+        1.7 NaN NaN NaN NaN 1.5 NaN;
+        0.4 1.41 0.4 1.2 0.7 0 0.2];
 
 
-Bz =   [1 NaN NaN NaN NaN NaN NaN;
-        3 3 NaN NaN NaN NaN NaN;
-        2 NaN 7 NaN NaN NaN NaN;
-        6 NaN NaN 9 NaN NaN NaN;
-        2 NaN NaN NaN 3 NaN NaN;
-        5 NaN NaN NaN NaN 5 NaN;
-        1 3 2 5 7 6 2];
-
+Bz =   [2 NaN NaN NaN NaN NaN NaN;
+        3.2 3.4 NaN NaN NaN NaN NaN;
+        2.7 NaN 3.5 NaN NaN NaN NaN;
+        3.6 NaN NaN 2.9 NaN NaN NaN;
+        2.8 NaN NaN NaN 2.2 NaN NaN;
+        3.5 NaN NaN NaN NaN 2.2 NaN;
+        1.9 3.1 2 3.2 3.7 2.6 3.2];
 
 a=0;
 BX = filltriangle(Bx, a);
